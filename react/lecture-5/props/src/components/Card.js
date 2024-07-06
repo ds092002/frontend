1ee662257d41ffba589e6113ff679e5427c187a7
Dhaval Solanki <dhavalas24@gmail.com>
2024-07-06 12:05:07 +0530

    06 July

--- react/lecture-5/props/src/components/Card.js
+++ react/lecture-5/props/src/components/Card.js
@@ -5,11 +5,11 @@ import { faBagShopping } from '@fortawesome/free-solid-svg-icons'
 const Card = (props) => {
   return (
 
-     <div className='rounded-xl shadow-lg m-5 hover:110 hover:shadow-xl transition duration-500 w-80'>
+     <div className='rounded-xl shadow-lg m-5 hover:scale-110 hover:shadow-xl transition duration-500 w-80'>
         <img src={props.image} alt="" className='object-cover w-80 h-80 rounded-t-xl' />
         <div className='px-4 py-3 w-80'>
             <p className='text-xs text-gray-400 mr-3 uppercase'>Brand</p>
-            <p className='font-bold truncate'>solanki dhavalo</p>
+            <p className='font-bold truncate'>Product Name</p>
             <div className='flex items-center'>
                <p className='my-3 text-lg font-semibold '>{props.curPrice}</p>
                <del className='text-sm ml-2 font-semibold text-gray-600'>{props.delPrice}</del>
