ecb39d2b63e3b11c678951d6befcca1b729932da
Dhaval Solanki <dhavalas24@gmail.com>
2024-07-11 11:57:58 +0530

    11 July

--- react/my-project/src/components/Navbar.jsx
+++ react/my-project/src/components/Navbar.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import styles from "./Navbar.module.css";
+import {useState} from "react"
 
 const Navbar = () => {
+    let initialState = 0;
+    const [count, setCount] = useState(initialState);
 
     const box = [
         {
@@ -18,8 +21,12 @@ const Navbar = () => {
         }
     ]
     return (
-        <div>
+        <div className="flex justify-center flex-col">
             <h1 className={box[0]}>This is CSS module staructure</h1>
+            <span>countNo:- {count}</span><br/>
+            <button onClick={() => setCount(count + 1)}>Increment</button>
+            <button onClick={() => setCount(count - 1)}>Decrement</button>
+            <button onClick={() => setCount(initialState)}>Reset</button>
         </div>
     );
 };
