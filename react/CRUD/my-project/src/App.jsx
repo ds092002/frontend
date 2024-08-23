3f3f19769e191e85f176d9452bf7547695355266
Dhaval Solanki <dhavalas24@gmail.com>
2024-08-23 21:47:03 +0530

    23 Aug

---
+++ react/CRUD/my-project/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import TableData from './components/Table'
+import Form from './components/Form'
+
+function App() {
+
+  return (
+    <>
+      <TableData/>
+      <Form/>
+    </>
+  )
+}
+
+export default App
