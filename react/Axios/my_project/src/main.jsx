5e7101f847b04de2d008110dd0b5e40218ad1892
Dhaval Solanki <dhavalas24@gmail.com>
2024-08-30 12:04:33 +0530

    30 Aug

--- react/Axios/my_project/src/main.jsx
+++ react/Axios/my_project/src/main.jsx
@@ -4,11 +4,13 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import Store from './redux/Store.jsx'
+import Axios from './axios/Axios.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider store={Store}>
+    {/* <Provider store={Store}>
     <App />
-    </Provider>
+    </Provider> */}
+    <Axios/>
   </StrictMode>,
 )
